34627862b116b1498f934267b53ee13362fd9c79
Jonathan Lamberts <jonathan.lamberts@gmail.com>
2019-02-05 18:11:04 -0500

    working on getting main to call submodules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+mod aoc;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +12,10 @@ fn main() {
         println!("Got arg {}", args[1]);
 
     let main_arg = &args[1];
+    let foo = [1,2,3]
 
         match &main_arg[..] {
+            "day1" => aoc::day1::calculate_chronal_frequency(&foo)
             "foobar" => println!("got dat foobar"),
             "baz" => println!("got dat baz"),
             _ => println!("got something else"),
